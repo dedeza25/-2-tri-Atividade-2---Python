9e28d59bc2737bce48f8b5d845a6d9f3b29d3eec
dedeza25 <andrezanascimento@colegiopaulodetarso.com.br>
2025-05-14 23:00:28 +0000

    3

---
+++ ex3.py
@@ -0,0 +1,4 @@
+for a in range(1, 6):  # loop para o primeiro número (a)
+    for b in range(1, 6):  # loop para o segundo número (b)
+        resultado = a * b
+        print(f"{a} x {b} = {resultado}")
